bad6593ee14231d84fd6c45af03327885374daf4
moe18 <chabotmoe1@gmail.com>
2021-04-27 00:33:28 -0400

    got financial data from yahoo

--- Stratagy-pipeline/Data-pipeline/get_data.py
+++ Stratagy-pipeline/Data-pipeline/get_data.py
@@ -1,4 +1,5 @@
-
+import pandas as pd
+from yahooquery import Ticker
 
 class GetFinData:
     """
@@ -10,3 +11,34 @@ class GetFinData:
     """
     def __init__(self):
         self.data = []
+        self.stocks = ['']
+        self.tickers = ''
+        self.start_date = ''
+        self.end_date = ''
+
+    def make_ticker_yahoo(self):
+        self.tickers = Ticker(self.stocks, asynchronous=True)
+
+    def get_hist_data_yahoo(self, period='1y', interval='1d'):
+        hist_data = self.tickers.history(period='1y', interval='1d')
+        return hist_data
+
+    def get_balance_sheet_data_yahoo(self, freq='a'):
+        bal_data = self.tickers.balance_sheet(frequency=freq)
+        return bal_data
+
+    def get_cash_flow_data_yahoo(self, freq='a'):
+        cash_data = self.tickers.cash_flow(frequency=freq)
+        return cash_data
+
+    def get_income_statement_data_yahoo(self, freq='a'):
+        income_data = self.tickers.income_statement(frequency=freq)
+        return income_data
+
+    def get_valuation_measures_data_yahoo(self):
+        val_measures = self.tickers.valuation_measures
+        return val_measures
+
+
+
+
